636f40c26e8bea8675826b53d84bffac62fb7bf1
Brandon140dot <bavatarmagic1@gmail.com>
2026-01-07 12:00:56 -0500

    Implement board creation and display functions
    Added functions to create and display a game board.

---
+++ version 2 .py
@@ -0,0 +1,11 @@
+def create_board():
+  return [[ 1,2,3], [4,5,6],[7,8,9]
+  
+def display_board(board):
+  for row in board:
+    print(row)
+    
+    
+    
+  Board = create_board()
+  display_board(board)
